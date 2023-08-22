dbbd66ab6a79f6743ecd07d0031a173ee65d6796
André Albuquerque <97198432+andre-albuquerque01@users.noreply.github.com>
2023-08-22 09:00:14 -0300

    Update LivroController.java

--- springboot/src/main/java/controller/LivroController.java
+++ springboot/src/main/java/controller/LivroController.java
@@ -3,6 +3,7 @@ package controller;
 import entity.Categoria;
 import entity.Livro;
 import entity.Modelo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.*;
@@ -11,8 +12,9 @@ import org.springframework.stereotype.Controller;
 
 @Controller
     @ResponseBody
-public class LivroController {
     @RequestMapping("/livros")
+public class LivroController {
+    @GetMapping("/livros")
     public List<Livro> listar(){
         Livro livro = new Livro(123L, "Helo Word", "Hieat", Categoria.economia, Modelo.EBOOK);
 
